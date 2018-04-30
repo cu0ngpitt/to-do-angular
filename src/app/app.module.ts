8b77a61fdf8fed786543dc1069ae26a783e2feee
Cuong Vo <36747471+cu0ngpitt@users.noreply.github.com>
2018-04-30 12:32:37 -0500

    Provided my list service to the main module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,11 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 
 import { AppComponent } from './app.component';
 import { ListComponent } from './list/list.component';
+import { ListService } from './list.service';
 
 
 @NgModule({
@@ -14,7 +16,7 @@ import { ListComponent } from './list/list.component';
   imports: [
     BrowserModule
   ],
-  providers: [],
+  providers: [ListService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
